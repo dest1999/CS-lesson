9d00345e09455c288b7c1f6d7aa8c40e4c759eaf
Михаил Савин <serv_all@mail.ru>
2019-12-01 17:13:50 +0300

    сделана задача 6 из лекции 2 (задача по хорошим числам)

--- goodNumbers/Program.cs
+++ goodNumbers/Program.cs
@@ -10,26 +10,41 @@ namespace goodNumbers
 {
     class Program
     {
+        static bool IsTheNumberIsGood (int i)
+        {
+            int tmpi = i, sumOfNumbers = 0;
+
+            do //в этом цикле парсим сумму чисел
+            {
+                sumOfNumbers += tmpi % 10;
+                tmpi /= 10;
+            } while (tmpi != 0);
+
+            if (i % sumOfNumbers == 0) //проверяем делится ли число нацело на сумму своих составляющих
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
         static void Main(string[] args)
         {
+            int qtyOfGoodNumbers = 0;
             DateTime start = DateTime.Now;
 
-            for (int i = 0; i < 1000; i++)
+            for (int i = 1; i < 1000000000; i++)
             {
-
-
-
-
-
+                if (IsTheNumberIsGood(i))
+                {
+                    qtyOfGoodNumbers++;
+                    if (i % 1000000 == 0) Console.Write($"Чисел найдено: {qtyOfGoodNumbers}\r");
+                }
             }
 
-
-
-
-
-
-
-            Console.WriteLine($"Программа отработала {DateTime.Now - start}");
+            Console.WriteLine($"Программа отработала {DateTime.Now - start}, было найдено {qtyOfGoodNumbers} чисел");
             Console.ReadKey();
         }
     }

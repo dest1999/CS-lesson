167a93cd13d208d8a49fbd2150171969c47b1180
Михаил Савин <serv_all@mail.ru>
2019-12-05 01:35:58 +0300

    Урок 3 задание 1 сделано

--- Complex/Program.cs
+++ Complex/Program.cs
@@ -38,11 +38,10 @@ namespace Complex
             return y;
         }
 
-        public string ToStr()
+        public override string ToString()
         {
-            return re + "+" + im + "i";
+            return re + "+" + im + "i str";
         }
-
     }
 
     class ComplexClass
@@ -52,9 +51,32 @@ namespace Complex
 
         public ComplexClass Plus(ComplexClass x)
         {
-            ComplexClass x = new ComplexClass();
+            ComplexClass y = new ComplexClass();
+            y.re = x.re + this.re;
+            y.im = x.im + this.im;
+            return y;
+        }
 
-            return 0;
+        public ComplexClass Minus(ComplexClass x)
+        {
+            ComplexClass y = new ComplexClass();
+            y.im = this.im - x.im;
+            y.re = this.re - x.re;
+            return y;
+        }
+
+        public ComplexClass Multi (ComplexClass x)
+        {
+            ComplexClass y = new ComplexClass();
+            y.re = this.re * x.re - this.im * x.im;
+            y.im = this.re * x.im + this.im * x.re;
+            return y;
+        }
+
+
+        public override string ToString()
+        {
+            return this.re + "+" + this.im + "i cls";
         }
 
 
@@ -65,23 +87,60 @@ namespace Complex
         static void Main(string[] args)
         {
             Complex cmplx1, cmplx2;
-            cmplx1.re = 1;
-            cmplx1.im = 1;
-            cmplx2.re = 2;
-            cmplx2.im = 2;
+            ComplexClass cc1 = new ComplexClass();
+            ComplexClass cc2 = new ComplexClass();
 
+            cc1.re = cmplx1.re = 1;
+            cc1.im = cmplx1.im = 1;
+            cc2.re = cmplx2.re = 3;
+            cc2.im = cmplx2.im = -9;
+
+            #region Check
             Complex result = cmplx1.Plus(cmplx2);
-            Console.WriteLine(result.ToStr());
+            Console.WriteLine(result.ToString());
+
+            ComplexClass resultCls = cc1.Plus(cc2);
+            Console.WriteLine(resultCls.ToString());
+
             result = cmplx1.Minus(cmplx2);
-            Console.WriteLine(result.ToStr());
+            Console.WriteLine(result.ToString());
+
+            resultCls = cc1.Minus(cc2);
+            Console.WriteLine(resultCls.ToString());
 
             result = cmplx1.Multi(cmplx2);
-            Console.WriteLine(result.ToStr());
+            Console.WriteLine(result.ToString());
 
+            resultCls = cc1.Multi(cc2);
+            Console.WriteLine(resultCls.ToString());
+            #endregion
+            Console.WriteLine("\nВыберите действие с числами: + - *");
+            ConsoleKeyInfo key = Console.ReadKey();
+            Console.WriteLine();
+            
+            switch (key.KeyChar.ToString())
+            {
+                case "+":
+                    resultCls = cc1.Plus(cc2);
+                    Console.WriteLine(resultCls.ToString());
+                    break;
+                case "-":
+                    resultCls = cc1.Minus(cc2);
+                    Console.WriteLine(resultCls.ToString());
+                    break;
+                case "*":
+                    resultCls = cc1.Multi(cc2);
+                    Console.WriteLine(resultCls.ToString());
+                    break;
+                default:
+                    Console.WriteLine("Вы нажали что-то не то");
+                    break;
+            }
+            
+            
 
-
-
-
+            
+            
             Console.ReadKey();
         }
     }

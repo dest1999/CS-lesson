d6b304d138c1501df1ca31a19cef863d58cc67ca
OlegSvrk <serv_all@mail.ru>
2019-12-22 01:24:36 +0300

    ДЗ к уроку 8: решены задачи 1 и 2

---
+++ tmp/refl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+/*
+ * С помощью рефлексии выведите все свойства структуры DateTime
+ * 
+ * coded by Олег Саврик
+ */
+namespace reflectionDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PropertyInfo[] properties;
+            Type type = typeof(DateTime);
+            properties = type.GetProperties();
+            Console.WriteLine($"Свойства {type}:\n");
+            foreach (var item in properties)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
